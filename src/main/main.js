59f2c873b1f10af2257af9269f66952380f3d065
thesoftwork15 <thesoftwork15@gmail.com>
2020-11-21 14:47:13 +0530

    new changes added

--- src/main/main.js
+++ src/main/main.js
@@ -68,38 +68,38 @@ const Main = props => {
                             <h4>Filters</h4>
                             <div className="filter-btnBx">
                                 <h6>Launch Year</h6>
-                                <a href="javascript:void(0)" className={ filter.year == '2006' ? 'active' : ''} onClick={ () => dataFilter(2006, 'year')}>2006</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2007' ? 'active' : ''} onClick={ () => dataFilter(2007, 'year')}>2007</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2008' ? 'active' : ''} onClick={ () => dataFilter(2008, 'year')}>2008</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2009' ? 'active' : ''} onClick={ () => dataFilter(2009, 'year')}>2009</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2010' ? 'active' : ''} onClick={ () => dataFilter(2010, 'year')}>2010</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2011' ? 'active' : ''} onClick={ () => dataFilter(2011, 'year')}>2011</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2012' ? 'active' : ''} onClick={ () => dataFilter(2012, 'year')}>2012</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2013' ? 'active' : ''} onClick={ () => dataFilter(2013, 'year')}>2013</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2014' ? 'active' : ''} onClick={ () => dataFilter(2014, 'year')}>2014</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2015' ? 'active' : ''} onClick={ () => dataFilter(2015, 'year')}>2015</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2016' ? 'active' : ''} onClick={ () => dataFilter(2016, 'year')}>2016</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2017' ? 'active' : ''} onClick={ () => dataFilter(2017, 'year')}>2017</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2018' ? 'active' : ''} onClick={ () => dataFilter(2018, 'year')}>2018</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2019' ? 'active' : ''} onClick={ () => dataFilter(2019, 'year')}>2019</a>
-                                <a href="javascript:void(0)" className={ filter.year == '2020' ? 'active' : ''} onClick={ () => dataFilter(2020, 'year')}>2020</a>
+                                <a className={ filter.year == '2006' ? 'active' : ''} onClick={ () => dataFilter(2006, 'year')}>2006</a>
+                                <a className={ filter.year == '2007' ? 'active' : ''} onClick={ () => dataFilter(2007, 'year')}>2007</a>
+                                <a className={ filter.year == '2008' ? 'active' : ''} onClick={ () => dataFilter(2008, 'year')}>2008</a>
+                                <a className={ filter.year == '2009' ? 'active' : ''} onClick={ () => dataFilter(2009, 'year')}>2009</a>
+                                <a className={ filter.year == '2010' ? 'active' : ''} onClick={ () => dataFilter(2010, 'year')}>2010</a>
+                                <a className={ filter.year == '2011' ? 'active' : ''} onClick={ () => dataFilter(2011, 'year')}>2011</a>
+                                <a className={ filter.year == '2012' ? 'active' : ''} onClick={ () => dataFilter(2012, 'year')}>2012</a>
+                                <a className={ filter.year == '2013' ? 'active' : ''} onClick={ () => dataFilter(2013, 'year')}>2013</a>
+                                <a className={ filter.year == '2014' ? 'active' : ''} onClick={ () => dataFilter(2014, 'year')}>2014</a>
+                                <a className={ filter.year == '2015' ? 'active' : ''} onClick={ () => dataFilter(2015, 'year')}>2015</a>
+                                <a className={ filter.year == '2016' ? 'active' : ''} onClick={ () => dataFilter(2016, 'year')}>2016</a>
+                                <a className={ filter.year == '2017' ? 'active' : ''} onClick={ () => dataFilter(2017, 'year')}>2017</a>
+                                <a className={ filter.year == '2018' ? 'active' : ''} onClick={ () => dataFilter(2018, 'year')}>2018</a>
+                                <a className={ filter.year == '2019' ? 'active' : ''} onClick={ () => dataFilter(2019, 'year')}>2019</a>
+                                <a className={ filter.year == '2020' ? 'active' : ''} onClick={ () => dataFilter(2020, 'year')}>2020</a>
                             </div>
 
                             <div className="filter-btnBx">
                                 <h6>Successful Launch</h6>
-                                <a href="javascript:void(0)" className={ filter.launch_success == true ? 'active' : ''} onClick={ () => dataFilter(true, 'launch_success')}>True</a>
-                                <a href="javascript:void(0)" className={ filter.launch_success == false ? 'active' : ''} onClick={ () => dataFilter(false, 'launch_success')}>False</a>
+                                <a className={ filter.launch_success == true ? 'active' : ''} onClick={ () => dataFilter(true, 'launch_success')}>True</a>
+                                <a className={ filter.launch_success == false ? 'active' : ''} onClick={ () => dataFilter(false, 'launch_success')}>False</a>
                             </div>
 
                             <div className="filter-btnBx">
                                 <h6>Successful Landing</h6>
-                                <a href="javascript:void(0)" className={ filter.land_success == true ? 'active' : ''} onClick={ () => dataFilter(true, 'land_success')}>True</a>
-                                <a href="javascript:void(0)" className={ filter.land_success == false ? 'active' : ''} onClick={ () => dataFilter(false, 'land_success')}>False</a>
+                                <a className={ filter.land_success == true ? 'active' : ''} onClick={ () => dataFilter(true, 'land_success')}>True</a>
+                                <a className={ filter.land_success == false ? 'active' : ''} onClick={ () => dataFilter(false, 'land_success')}>False</a>
                             </div>
 
                             <div className="filter-btnBx">
                                 <h6>Reset</h6>
-                                <a href="javascript:void(0)" onClick={ () => dataFilter('', 'reset')}>Reset</a>
+                                <a onClick={ () => dataFilter('', 'reset')}>Reset</a>
                             </div>
                             </CustomScroller>                            
                             
@@ -111,7 +111,7 @@ const Main = props => {
                                         <img src={res.links.mission_patch} alt="spaceX" title="spaceX" />
                                     </div>
                                     <div className="spaceX-itemInfo">
-                                        <h6><a href="javascript:void(0)">{res.mission_name} #{res.flight_number}</a></h6>
+                                        <h6><a title={res.mission_name}>{res.mission_name} #{res.flight_number}</a></h6>
                                         <p><b>Mission Id: </b>{res.mission_id != '' ? res.mission_id : 'NA'}</p>
                                         <p><b>Launch Year: </b>{res.launch_year}</p>
                                         <p><b>Successful Launch: </b>{res.launch_success ? 'true' : 'false'}</p>
